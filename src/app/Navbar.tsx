809aa7abbcb67116e39d2ff5094f9d8b51c8d5e8
Agiprada <78854969+Agiprada@users.noreply.github.com>
2024-01-08 22:34:18 +0800

    change bg navbar

--- src/app/Navbar.tsx
+++ src/app/Navbar.tsx
@@ -3,7 +3,7 @@ import React from "react";
 
 export default function Navbar() {
   return (
-    <nav className="flex bg-indigo-800 py-2 px-5">
+    <nav className="flex bg-emerald-500 py-2 px-5">
       <h1 className="text-2xl text-white">Navbar</h1>
       <ul className="flex gap-5 text-white items-center">
         <li className="ml-5">
