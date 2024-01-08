fb87996891d6c9b234eecb8c80d39aa831d50511
Agiprada <78854969+Agiprada@users.noreply.github.com>
2024-01-08 22:28:02 +0800

    first commit

---
+++ src/app/Navbar.tsx
@@ -0,0 +1,24 @@
+import Link from "next/link";
+import React from "react";
+
+export default function Navbar() {
+  return (
+    <nav className="flex bg-indigo-800 py-2 px-5">
+      <h1 className="text-2xl text-white">Navbar</h1>
+      <ul className="flex gap-5 text-white items-center">
+        <li className="ml-5">
+          <Link href="/">Atomic</Link>
+        </li>
+        <li>
+          <Link href="/about">about</Link>
+        </li>
+        <li>
+          <Link href="/about/profile">profile</Link>
+        </li>
+        <Link href="*">
+          <li>logout</li>
+        </Link>
+      </ul>
+    </nav>
+  );
+}
---
+++ src/app/about/layout.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function AboutLayout() {
+  return (
+    <aside className="fixed left-0 top-12 z-10 h-screen w-32 bg-indigo-500">
+      <ul className="flex flex-col text-white items-center p-5 gap-4">
+        <li>Home</li>
+        <li>Home</li>
+        <li>Home</li>
+      </ul>
+    </aside>
+  );
+}
